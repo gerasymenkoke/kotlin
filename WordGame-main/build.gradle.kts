13ca53e0afef42b2f1e0dc23e2951f72b34e44e0
gerasymenkoke <104980001+gerasymenkoke@users.noreply.github.com>
2023-04-16 21:49:14 +0300

    Update build.gradle.kts

--- WordGame-main/build.gradle.kts
+++ WordGame-main/build.gradle.kts
@@ -156,8 +156,8 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_8
+        targetCompatibility = JavaVersion.VERSION_8
     }
 
     packagingOptions {
